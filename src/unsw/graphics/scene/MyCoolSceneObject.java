da5bb611b8d15c670e788c707030d29fd69e5ed4
HelloRicky <ricky.zheng2@gmail.com>
2018-08-20 21:34:33 +1000

    finish myCoolSceneObject

--- src/unsw/graphics/scene/MyCoolSceneObject.java
+++ src/unsw/graphics/scene/MyCoolSceneObject.java
@@ -11,27 +11,73 @@ import unsw.graphics.geometry.Polygon2D;
 public class MyCoolSceneObject extends SceneObject {
 
 	PolygonalSceneObject p;
-	
 	public MyCoolSceneObject(SceneObject parent) {
         super(parent);
         // TODO: Write your scene object
-
-        Polygon2D poly = new Polygon2D(0,0, 1,1, 0,1);
+        float s = 0.8f;
+        
+        // face shape
+        Polygon2D poly = new Polygon2D(s,-s, s,s, -s,s, -s,-s);
         p = new PolygonalSceneObject(parent, poly, Color.GREEN, Color.YELLOW );
-        p.rotate(45);
         
-        //Create a line that is a child of polygon p
-        Color lineCol2 = new Color(0.5f, 0.5f, 1);
-        LineSceneObject l2 = new LineSceneObject(p, lineCol2);     
-        l2.setPosition(-1, 0);
-      
+
+        // nose
+        set_nose(p);
+
+        // eyes
+        set_eye(p, 1);		// right
+        set_eye(p, -1);		// left
         
-        //Create a circle that is a child of polygon p       
-        CircularSceneObject c2 = new CircularSceneObject(p, 0.25f, Color.WHITE, Color.WHITE);     
-        c2.translate(-1,0);
+        // mouth
+        set_mouth(p);
+        
+        // eyebrows
+        set_eyebrows(p,1); 	// right
+        set_eyebrows(p,-1);	// left
+        
         
     }
     
+	public void set_nose(SceneObject frame){
+        Polygon2D nose = new Polygon2D(0.5f,0, 0,1, -0.5f,0);
+        PolygonalSceneObject n = new PolygonalSceneObject(frame, nose, Color.YELLOW, null);
+        n.translate(0, 0.1f);
+        n.scale(0.1f);
+		
+	}
+	
+	
+	public void set_eye(SceneObject frame, float side){
+		CircularSceneObject e1 = new CircularSceneObject(frame, 0.25f, Color.WHITE, Color.WHITE);
+        e1.translate(side*0.5f,0.1f);
+		
+        CircularSceneObject e2 = new CircularSceneObject(e1, 0.25f, Color.BLACK, Color.WHITE);
+        e2.translate(0, 0.1f);
+        e2.scale(0.5f);
+
+	}
+	
+	public void set_mouth(SceneObject frame){
+		float s = 0.6f;
+		Polygon2D poly = new Polygon2D(s,0, s, -0.9f*s, 0,-s, -s,-0.9f*s, -s,0);
+		PolygonalSceneObject p = new PolygonalSceneObject(frame, poly, Color.PINK, Color.YELLOW );
+		p.translate(0, -0.1f);
+		
+		Color lineCol2 = new Color(0.5f, 0.5f, 1);
+		LineSceneObject l1 = new LineSceneObject(p, lineCol2);     
+        l1.setPosition(-s, 0.5f*-s);
+	}
+	
+	public void set_eyebrows(SceneObject frame, float side){
+		float s = 0.2f;
+		float l = 0.4f;
+		float h = 0.5f;
+
+		Polygon2D poly = new Polygon2D(side*s,s+h, 	side*(s+l),s+h, 	side*(s+l), 0.9f*s+h, 	side*s,0.7f*s+h);
+		PolygonalSceneObject p = new PolygonalSceneObject(frame, poly, Color.PINK, Color.YELLOW );
+
+	}
+	
 	public void translate(float x, float y) {
 		p.setPosition(x, y);
 	}
