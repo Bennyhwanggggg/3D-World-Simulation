4579bbb8b327950aceb91af50e89810ca1e0f9c9
Ricky <ricky.zheng2@gmail.com>
2018-09-10 16:54:40 +1000

    v0.18

--- src/unsw/graphics/Point3DBuffer.java
+++ src/unsw/graphics/Point3DBuffer.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 import com.jogamp.opengl.util.GLBuffers;
 
-import unsw.graphics.geometry.Point2D;
+import unsw.graphics.geometry.Point3D;
 
 /**
  * A buffer of points. Can be passed to OpenGL commands that expect buffers by
@@ -18,7 +18,7 @@ import unsw.graphics.geometry.Point2D;
  * @author Robert Clifton-Everest
  *
  */
-public class Point2DBuffer {
+public class Point3DBuffer {
 
     private FloatBuffer floatBuffer;
 
@@ -29,13 +29,13 @@ public class Point2DBuffer {
      * 
      * @param capacity
      */
-    public Point2DBuffer(int capacity) {
+    public Point3DBuffer(int capacity) {
         // Buffer stores pairs of floats
         this.capacity = capacity;
-        floatBuffer = GLBuffers.newDirectFloatBuffer(capacity * 2);
+        floatBuffer = GLBuffers.newDirectFloatBuffer(capacity * 3);
     }
 
-    public Point2DBuffer(List<Point2D> points) {
+    public Point3DBuffer(List<Point3D> points) {
         this(points.size());
         for (int i = 0; i < capacity; i++) {
             put(i, points.get(i));
@@ -43,27 +43,28 @@ public class Point2DBuffer {
     }
 
     /**
-     * Add a {@link Point2D} to the buffer at the given index.
+     * Add a {@link Point3D} to the buffer at the given index.
      * 
      * @param index
      * @param p
      */
-    public void put(int index, Point2D p) {
-        put(index, p.getX(), p.getY());
+    public void put(int index, Point3D p) {
+        put(index, p.getX(), p.getY(), p.getZ());
     }
 
     /**
-     * Add a point (given as an x-y coordinate) to the buffer at the given
+     * Add a point (given as an x-y-z coordinate) to the buffer at the given
      * index.
      * 
      * @param index
      * @param x
      * @param y
      */
-    public void put(int index, float x, float y) {
+    public void put(int index, float x, float y, float z) {
         if (index >= 0 && index < capacity) {
-            floatBuffer.put(index * 2, x);
-            floatBuffer.put(index * 2 + 1, y);
+            floatBuffer.put(index * 3, x);
+            floatBuffer.put(index * 3 + 1, y);
+            floatBuffer.put(index * 3 + 2, z);
         } else {
             throw new IndexOutOfBoundsException(
                     "index: " + index + ", capacity: " + capacity);
@@ -76,6 +77,10 @@ public class Point2DBuffer {
 
     public int capacity() {
         return capacity;
+    }
+
+    public Point3D get(int i) {
+        return new Point3D(floatBuffer.get(i*3), floatBuffer.get(i*3 + 1), floatBuffer.get(i*3 + 2));
     }
 
 }
